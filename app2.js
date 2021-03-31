9281444bc45f16b7c535b9429d572b9fb8a45e9f
Tiago Mendes <tetigo@gmail.com>
2021-03-30 23:21:04 -0300

    props 02

--- app2.js
+++ app2.js
@@ -1,9 +1,29 @@
 Vue.component('plan', {
     template: '#plan-template',
-    props: ['name']
+    // props: ['name'],
+    
+    // props: {
+    //     name: String,
+    // },
+
+    props: {
+        name: {
+            type: String,
+            default: 'Tiago The Great',
+            required: true
+        }
+    }
 })
 
 
 new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        plans: [
+            'The Hacker',
+            'The Curious',
+            'The Single',
+            'The Addict'
+        ]
+    }
 })
